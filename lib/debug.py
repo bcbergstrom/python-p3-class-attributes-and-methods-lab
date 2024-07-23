12341d6fbcefbd3892d345e372f55ec890fc0b2f
Benton Bergstrom <bbergstrom0524@gmail.com>
2024-07-23 16:14:13 -0600

    final commit

---
+++ lib/debug.py
@@ -0,0 +1,8 @@
+from song import Song
+
+if __name__ == "__main__":
+    print("hoi")
+    Song("99 Problems", "Jay Z", "Rap")
+    Song("Halo", "Beyonce", "Pop")
+    Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+    
